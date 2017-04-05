7a836d9b013d926f1fd0ec06438f267c74626f5a
dieulex <lexuandieuit94@gmail.com>
2017-04-05 18:22:29 +0700

    create controller for Index, news, routes layout

--- app/Http/Controllers/Danabus/NewsController.php
+++ app/Http/Controllers/Danabus/NewsController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers\Danabus;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\News;
 
 class NewsController extends Controller
 {
-    public function news(){
-    	return view('danabus.news.news');
+    public function news($slug, $id){
+    	$news = News::find($id);
+    	return view('danabus.news.news', ['news' => $news]);
     }
 }
--- app/Http/Controllers/Danabus/RoutesController.php
+++ app/Http/Controllers/Danabus/RoutesController.php
@@ -4,13 +4,18 @@ namespace App\Http\Controllers\Danabus;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
+use App\Route;
 
 class RoutesController extends Controller
 {
     public function routes(){
-    	return view('danabus.routes.tuyen');
+        $ngoaithanh = DB::table('routes')->where('type', '=',1)->get();
+        $noithanh = DB::table('routes')->where('type', '=',0)->get();
+    	return view('danabus.routes.tuyen',['ngoaithanh' => $ngoaithanh, 'noithanh' => $noithanh]);
     }
-    public function tuyencontent(){
-    	return view('danabus.routes.tuyencontent');
+    public function tuyencontent($slug, $id){
+    	$routes = Route::find($id);
+    	return view('danabus.routes.tuyencontent',['routes' => $routes]);
     }
 }
