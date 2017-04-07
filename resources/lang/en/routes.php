304d17712879127b6897cf3214232268fe0b1d49
dieulex <lexuandieuit94@gmail.com>
2017-04-07 18:24:25 +0700

    Upload template

---
+++ resources/lang/en/routes.php
@@ -0,0 +1,15 @@
+<?php
+    return [
+        'title' => 'Danh sách các tuyến xe',
+        'supurban' => 'Tuyến xe buýt liên tỉnh',
+        'urban' => 'Tuyến xe buýt  nội thành Đà Nẵng',
+        'map' => 'Bản đồ ',
+        'go' => 'Hành trình lượt đi',
+        'back' => 'Hành trình lượt về:',
+        'frequency' => 'Tần xuất: ',
+        'frequency_peak' => 'Tần suất giờ cao điểm ',
+        'time' => ' Thời gian hoạt động hàng ngày:    ',
+        'distance' => 'Khoảng Cách :'
+        
+
+    ];
