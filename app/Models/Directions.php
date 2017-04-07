42c190a169d552b9629b69c0d5e94e5583b7d616
at-tinhdang <tinhdt1992@gmail.com>
2017-04-07 13:06:09 +0700

    Fix model

---
+++ app/Models/Directions.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Directions extends Model
+{
+    protected $table = 'directions';
+    protected $fillable = [
+        'order', 'fee', 'status', 'time', 'route_id', 'stop_id'
+    ];
+    public $timestamps = true;
+
+    /**
+    * Get the stop record associated with the direction.
+    *
+    * @return direction
+    */
+    public function stop()
+    {
+        return $this->hasOne('App\Models\Stop');
+    }
+
+    /**
+     * Get parent that owns direction
+     *
+     * @return Route
+     */
+    public function routes()
+    {
+        return $this->belongsTo('App\Models\Route');
+    }
+}
