a601a20193c55413854fb163e56673ff4c4b7f51
dieulex <lexuandieuit94@gmail.com>
2017-04-10 16:23:59 +0700

    create construct for project

---
+++ resources/lang/en/routes.php
@@ -0,0 +1,16 @@
+<?php
+    return [
+        'title' => 'Danh sách các tuyến xe',
+        'supurban' => 'Tuyến xe buýt liên tỉnh',
+        'urban' => 'Tuyến xe buýt  nội thành Đà Nẵng',
+        'map' => 'Bản đồ ',
+        'go' => 'Hành trình lượt đi',
+        'back' => 'Hành trình lượt về:',
+        'frequency' => 'Tần xuất: ',
+        'frequency_peak' => 'Tần suất giờ cao điểm ',
+        'time' => ' Thời gian hoạt động hàng ngày:    ',
+        'distance' => 'Khoảng Cách :',
+        'routes' => 'Tuyến'
+        
+
+    ];
