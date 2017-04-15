809dc006391a40ee537ba7f29ba5947a6b5bac1c
dieulex <lexuandieuit94@gmail.com>
2017-04-15 22:09:50 +0700

    template for public page

---
+++ resources/lang/en/header.php
@@ -0,0 +1,10 @@
+<?php
+
+    return [
+        'web_name' => 'Danabus.com',
+        'home' => 'Trang chủ ',
+        'routes' => 'Tuyến xe ',
+        'search' => 'Tìm đường ',
+        'feedback' => 'Phản hổi'
+
+    ];
