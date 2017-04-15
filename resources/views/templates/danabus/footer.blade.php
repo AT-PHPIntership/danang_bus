6f325b3b6382eb7f0d090c10b3edddd6f1da3e71
dieulex <lexuandieuit94@gmail.com>
2017-04-15 22:09:50 +0700

    template for public page

---
+++ resources/views/templates/danabus/footer.blade.php
@@ -0,0 +1,7 @@
+<footer>
+      <div class="footer">
+        <p>{!! trans('footer.danang_bus') !!}</p>
+    </footer>
+    <script type="text/javascript" src="{{ asset('templates/danabus/js/app.js')}}"></script>
+  </body>
+</html>
