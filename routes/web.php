5a5ad8b1889dbf59837e29c003f956651bbcae85
vagrant <vagrant@homestead>
2017-04-20 07:30:14 +0000

    Fix user_admin

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::group(['namespace' => 'Admin', 'prefix' => 'admin'], function () {
     Route::get('/home', 'AdminController@index');
     Route::resource('/categories', 'CategoryController', ['as' => 'admin']);
     Route::resource('/news', 'NewsController', ['as' => 'admin']);
-    Route::resource('/users', 'UserController', ['as' => 'admin']);
+    Route::resource('/users', 'UserController', ['as' => 'admin', 'except' => 'show']);
     Route::resource('stops', 'StopController', ['as' => 'admin']);
     Route::Auth();
 });
