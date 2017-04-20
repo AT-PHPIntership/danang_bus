6498c53aa81919e7141153631e58e6b88b874738
vagrant <vagrant@homestead>
2017-04-20 01:31:11 +0000

    Fix edit_news

--- config/constant.php
+++ config/constant.php
@@ -1,5 +1,5 @@
 <?php
     return [
-        'path_upload_news' => 'upload/picture_news',
+        'path_upload_news' => 'upload/picture_news/',
         'path_remove_news' => '../public/upload/picture_news/',
     ];
